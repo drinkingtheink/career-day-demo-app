b00a363399fdc192ba72359eff8b15909b2179b8
drinkingtheink <jason.m.harrison@gmail.com>
2024-05-09 22:44:16 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Cypress Elementary Career Day 2024"/>
 </template>
 
 <script>
