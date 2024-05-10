77fdddcc8c6de8ac351abcd95c5af2cd6f62de29
drinkingtheink <jason.m.harrison@gmail.com>
2024-05-09 23:11:29 -0500

    draw with mouse

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <img class="banner" src="./assets/cheetah.jpeg">
   <HelloWorld msg="Cypress Elementary Career Day 2024"/>
+  <section class="img-capture">
+    <h2>Drawing with the Mouse</h2>
+    <canvas id="myCanvas" width="560" height="360" @mousedown="beginDrawing" @mousemove="keepDrawing" @mouseup="stopDrawing" />
+  </section>
 </template>
 
 <script>
@@ -10,7 +14,51 @@ export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  data() {
+    return {
+      x: 0,
+      y: 0,
+      isDrawing: false,
+      canvas: null,
+    }
+  },
+  methods: {
+    drawLine(x1, y1, x2, y2) {
+      let ctx = this.canvas;
+      ctx.beginPath();
+      ctx.strokeStyle = 'black';
+      ctx.lineWidth = 1;
+      ctx.moveTo(x1, y1);
+      ctx.lineTo(x2, y2);
+      ctx.stroke();
+      ctx.closePath();
+    },
+    beginDrawing(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+      this.isDrawing = true;
+    },
+    keepDrawing(e) {
+      if (this.isDrawing === true) {
+        this.drawLine(this.x, this.y, e.offsetX, e.offsetY);
+        this.x = e.offsetX;
+        this.y = e.offsetY;
+      }
+    },
+    stopDrawing(e) {
+      if (this.isDrawing === true) {
+        this.drawLine(this.x, this.y, e.offsetX, e.offsetY);
+        this.x = 0;
+        this.y = 0;
+        this.isDrawing = false;
+      }
+    }
+  },
+  mounted() {
+    var c = document.getElementById("myCanvas");
+    this.canvas = c.getContext('2d');
+  },
 }
 </script>
 
